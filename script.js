25c888fee68a21a01ffc9ed16b4c646a8357140e
VeronicaFM <31071240+VeronicaFM@users.noreply.github.com>
2019-05-03 11:34:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ $('h1').click(function(){
 var myIndex = 0;
 var y = document.getElementsByClassName("mySlidesText");
 carousel();
-var inter= 900
+var inter= 1100
  var set = setInterval(carousel, inter);
 
 function carousel() {
